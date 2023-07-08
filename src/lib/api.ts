6e827affac7b8faa691dcd3aaa07d1c8e923a3fa
usbbush <jeanbienvenusb@gmail.com>
2023-07-08 16:31:57 +0200

    Update api method

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,37 +1,88 @@
-const fetcher = async ({ url, method, body, json = true }) => {
+'use client';
+import clsx from 'clsx';
+
+export const BASE_URL = 'http://127.0.0.1:3001';
+export const API_URL = BASE_URL + '/api/v1';
+
+const fetcher = async ({url, method, body}) => {
   const res = await fetch(url, {
     method,
-    body: body && JSON.stringify(body),
+    ...(body && {body: JSON.stringify(body)}),
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
   });
 
   if (!res.ok) {
-    throw new Error("API Error");
+    console.log('Error thrown .....................');
+    throw new Error('API Error');
   }
+  console.log('ok');
+  console.log(res.headers);
+  localStorage.setItem('token', res.headers.get('Authorization'));
 
-  if (json) {
-    const data = await res.json();
-    return data;
-  }
+  const data = await res.json();
+  console.log('data found .....................' + data);
+  return data;
+  // }
 };
 
+const autFetcher = async ({url, method, body = null}) => {
+  // console.log(clsx(url, 'is the api url'));
+
+  const {data} = await fetch(url, {
+    method,
+    ...(body && {body: JSON.stringify(body)}),
+    headers: {
+      Accept: 'application/json',
+      Authorization: typeof window !== 'undefined' ? window.localStorage.getItem('token') : '',
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json()).then(data => {
+    return data;
+  }).catch(e => console.log(e));
+
+  // if (!res.ok) {
+  //   console.log('Error thrown .....................');
+  console.log(data)
+  //   throw new Error('API Error');
+  // }
+  ;
+  console.log('data found .....................' + data);
+  return data;
+};
 export const register = async (user) => {
   return fetcher({
-    url: "/api/register",
-    method: "POST",
+    url: clsx(BASE_URL, '/signup'),
+    method: 'POST',
     body: user,
-    json: false,
   });
 };
 
 export const signin = async (user) => {
   return fetcher({
-    url: "/api/signin",
-    method: "POST",
+    url: BASE_URL + '/login',
+    method: 'POST',
     body: user,
-    json: false,
   });
 };
+
+export function authFetcher(url) {
+  return fetch(url, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: typeof window !== 'undefined' ? window.localStorage.getItem('token') : '',
+      'Content-Type': 'application/json',
+    },
+  }).then(response => response.json()).then(result => result.data);
+}
+
+// export const getUsers = async () => {
+//   console.log('loading users .........');
+//   return authFetcher({
+//       url: API_URL + '/users',
+//       method: 'GET',
+//     }
+//   );
+// };
