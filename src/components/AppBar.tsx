fbe76a5d58cb24f05c0512a246d000b83f70cc77
usbbush <jeanbienvenusb@gmail.com>
2023-07-26 10:51:15 +0200

    Create an AppBar Component

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,10 @@
+import Card from '@/components/Card';
+import BackButton from '@/components/BackButton';
+
+export default function AppBar() {
+  return (
+    <div className="border-b ">
+      <div>App bar</div>
+    </div>
+  )
+}
