9c56786d64224e5b9564afa3a98e956d9822514d
usbbush <jeanbienvenusb@gmail.com>
2023-07-26 10:53:56 +0200

    Update component classname list

--- src/components/GlassPane.tsx
+++ src/components/GlassPane.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 const GlassPane = ({children, className}) => {
   return <div className={clsx(
-    'glass rounded-2xl border-solid border-2 border-gray-200',
+    'glass',
     className
   )}>
     {children}
