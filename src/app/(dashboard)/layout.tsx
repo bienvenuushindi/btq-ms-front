274b496a81359f60837905dfc664820eec5fe814
usbbush <jeanbienvenusb@gmail.com>
2023-07-26 11:02:42 +0200

    Update dashboard  layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,29 @@
-import '@/styles/global.css'
+import '@/styles/global.css';
 import GlassPane from '@/components/GlassPane';
+import Sidebar from '@/components/Sidebar';
+import AppBar from '@/components/AppBar';
 
 export default function DashboardRootLayout({children}) {
   return (
     <html lang="en">
     <head/>
-    <body className="h-screen w-screen rainbow-mesh p-6">
-    <GlassPane className="w-full h-full flex items-center justify-center">
-      {children}
-    </GlassPane>
+    <body>
+    <div className="h-screen w-screen rainbow-mesh p-6">
+      <div>
+      </div>
+      <GlassPane className="w-full h-full flex items-center justify-center">
+        <Sidebar/>
+        <div className="grow flex flex-col h-full items-center justify-center">
+          <div className="w-full">
+            <AppBar />
+          </div>
+          <div className="grow w-auto bg-white text-black m-2   overflow-y-scroll">
+            {children}
+          </div>
+        </div>
+
+      </GlassPane>
+    </div>
     </body>
     </html>
   );
