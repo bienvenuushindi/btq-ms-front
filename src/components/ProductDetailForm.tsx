e4e3a0cd460c49d4cfb6c84036f74ad04562f3c5
usbbush <jeanbienvenusb@gmail.com>
2023-08-14 19:34:05 +0200

    Use generic form

--- src/components/ProductDetailForm.tsx
+++ src/components/ProductDetailForm.tsx
@@ -1,15 +1,13 @@
 'use client';
-import {useCallback, useState} from 'react';
+import React, {useState} from 'react';
 import Card from '@/components/Card';
-import Input from '@/components/Input';
-import Button from '@/components/Button';
 import {useRouter, useParams} from 'next/navigation';
-import {sendPost} from '@/lib/api';
+import {send} from '@/lib/api';
+import Form from '@/components/Form';
 
 export const ProductDetailForm = () => {
-  const router = useRouter()
+  const router = useRouter();
   const path = useParams();
-  console.log(path.id);
   const initial = {
     size: '',
     expired_date: '',
@@ -17,35 +15,156 @@ export const ProductDetailForm = () => {
     dozen_price: 0.0,
     box_price: 0.0,
     dozen_units: 12,
-    box_units: 1
+    box_units: 1,
+    currency: '',
+    active: ''
   };
   const [formState, setFormState] = useState({...initial});
   const [error, setError] = useState('');
-  const handleSubmit = useCallback(async (e) => {
-      e.preventDefault();
-      const data = {
-        product_detail: formState
-      };
-      console.log(data);
-      try {
-        //submit promise
-        await sendPost('/products/' + path.id + '/product_details', data);
-        router.replace('/products/');
-      } catch (e) {
-        setError(`Could not create product`);
-      } finally {
-        setFormState({...initial});
+  const [photos, setPhotos] = useState([]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    Object.keys(formState).forEach((key) => {
+      formData.append(`product_detail[${key}]`, formState[key]);
+    });
+    for (let i = 0; i < photos.length; i++) {
+      formData.append('product_detail[images][]', photos[i]);
+    }
+    try {
+      //submit promise
+      await send('/products/' + path.id + '/product_details', formData);
+      router.replace('/products/' + path.id);
+    } catch (e) {
+      setError(`Could not create product`);
+    } finally {
+      setFormState({...initial});
+    }
+  };
+  const productDetailForm = [
+    {
+      label: 'Size',
+      required: true,
+      placeholder: 'Product Size',
+      value: formState.size,
+      name: 'size',
+      type: 'number',
+      input_type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, size: e.target.value}));
+      },
+    },
+    {
+      label: 'Expiration date',
+      required: true,
+      placeholder: 'Expired Date',
+      value: formState.expired_date,
+      name: 'expired_date',
+      input_type: 'date',
+      type: 'date',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, expired_date: e.target.value}));
+      },
+    },
+    {
+      label: 'Unit Price',
+      required: true,
+      placeholder: 'Unit Price',
+      value: formState.unit_price,
+      name: 'unit-price',
+      input_type: 'number',
+      type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, unit_price: e.target.value}));
+      },
+    },
+    {
+      label: 'Dozen Price',
+      required: true,
+      placeholder: 'Dozen Price',
+      value: formState.dozen_price,
+      name: 'dozen-price',
+      input_type: 'number',
+      type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, dozen_price: e.target.value}));
+      },
+    },
+    {
+      label: 'Box Price',
+      required: true,
+      placeholder: 'Box Price',
+      value: formState.dozen_price,
+      name: 'box-price',
+      input_type: 'number',
+      type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, box_price: e.target.value}));
+      },
+    },
+    {
+      label: 'Unit Qty in Dozen',
+      required: true,
+      placeholder: 'Dozen units',
+      value: formState.dozen_units,
+      name: 'dozen-units',
+      input_type: 'number',
+      type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, dozen_units: e.target.value}));
+      },
+    },
+    {
+      label: 'Unit Qty in Box',
+      required: true,
+      placeholder: 'Box Units',
+      value: formState.dozen_price,
+      name: 'box-units',
+      input_type: 'number',
+      type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, box_units: e.target.value}));
+      },
+    },
+    {
+      label: 'Choose currency',
+      placeholder: 'Select Currency',
+      name: 'country',
+      input_type: 'radio',
+      className: '',
+      options: ['fc', 'ugx', 'usd'],
+      action: (e) => {
+        setFormState((s) => ({...s, currency: e.target.value}));
       }
     },
-    [
-      formState.size,
-      formState.expired_date,
-      formState.unit_price,
-      formState.dozen_price,
-      formState.box_price,
-      formState.dozen_units,
-      formState.box_units,
-    ]);
+    {
+      label: 'Photos',
+      input_type: 'image-file',
+      image_props: {photos, setPhotos}
+    },
+    {
+      label: 'Status',
+      input_type: 'checkbox',
+      className: '',
+      checked: formState.active,
+      action: (e) => {
+        setFormState((s) => ({...s, active: !formState.active}));
+      }
+    },
+    {
+      input_type: 'button',
+      className: '',
+      type: 'submit',
+      placeholder: 'Submit'
+    }
+  ];
 
   const content = {
     header: 'Create a product',
@@ -60,88 +179,7 @@ export const ProductDetailForm = () => {
             <h2 className="text-3xl mb-2 text-black">{content.header}</h2>
             <p className="tex-lg text-black/25">{content.subheader}</p>
           </div>
-          <form onSubmit={handleSubmit} className="py-10 w-full">
-            <div className="mb-8">
-              <Input required
-                     placeholder="Size"
-                     value={formState.size}
-                     type="text"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, size: e.target.value}))
-                     }/>
-            </div>
-            <div className="mb-8">
-              <Input required
-                     placeholder="Expired Date"
-                     value={formState.expired_date}
-                     type="date"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, expired_date: e.target.value}))
-                     }/>
-            </div>
-            <div className="mb-8">
-              <Input required
-                     placeholder="Price of Unit"
-                     value={formState.unit_price}
-                     type="number"
-                     step="0.1"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, unit_price: e.target.value}))}
-              />
-            </div>
-            <div className="mb-8">
-              <Input required
-                     placeholder="Price of a dozen"
-                     value={formState.dozen_price}
-                     type="number"
-                     step="0.1"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, dozen_price: e.target.value}))}
-              />
-            </div>
-            <div className="mb-8">
-              <Input required
-                     placeholder="Price of a box"
-                     value={formState.box_price}
-                     type="number"
-                     step="0.1"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, box_price: e.target.value}))}
-              />
-            </div>
-            <div className="mb-8">
-              <Input required
-                     placeholder="How many item in a dozen"
-                     value={formState.dozen_units}
-                     type="number"
-                     step="1"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, dozen_units: e.target.value}))}
-              />
-            </div>
-            <div className="mb-8">
-              <Input required
-                     placeholder="How many item in a box"
-                     value={formState.box_units}
-                     type="number"
-                     step="1"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, box_units: e.target.value}))}
-              />
-            </div>
-            <div>
-              <Button type="submit" intent="secondary">
-                {content.buttonText}
-              </Button>
-            </div>
-          </form>
+          <Form fields={productDetailForm} handleSubmit={handleSubmit}/>
         </div>
       </Card>
     </>
