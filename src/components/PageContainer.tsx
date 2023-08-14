3714c99626dd32c6b55287d8f801152b9834ac79
usbbush <jeanbienvenusb@gmail.com>
2023-08-14 19:51:25 +0200

    Apply sidebar context to dashboard

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,16 @@
+'use client'
+import {SidebarContext} from '@/components/sidebar/SidebarContainer';
+import {useState} from 'react';
+export const sidebarInitial = {state: false, target: ''}
+export default function PageContainer({children}){
+  const [openBar, setOpenBar] = useState(sidebarInitial)
+  const [sidebarData, setSidebarData] = useState({})
+  return (
+    <>
+      <SidebarContext.Provider value={{openBar,setOpenBar, sidebarData,setSidebarData }}>
+        {children}
+      </SidebarContext.Provider>
+    </>
+  )
+
+}
