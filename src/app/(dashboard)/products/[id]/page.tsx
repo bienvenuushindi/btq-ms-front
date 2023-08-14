f6d7b4dc2c87e15dde4814faf2f56a774db30327
usbbush <jeanbienvenusb@gmail.com>
2023-08-14 19:58:09 +0200

    Show product details

--- src/app/(dashboard)/products/[id]/page.tsx
+++ src/app/(dashboard)/products/[id]/page.tsx
@@ -1,21 +1,53 @@
 'use client';
-import useSWR from 'swr';
-import {API_URL, authFetcher} from '@/lib/api';
-import {useParams, usePathname} from 'next/navigation';
+import {useParams} from 'next/navigation';
+import {useProduct} from '@/app/hooks/useProduct';
+import ProductItem from '@/components/products/ProductItem';
+import ButtonLink from '@/components/ButtonLink';
+import Card from '@/components/Card';
+import {ProductDetailsTable} from '@/components/products/ProductDetailsTable';
+import {SidebarContainer, SidebarContext} from '@/components/sidebar/SidebarContainer';
+import SidebarContentSelector from '@/components/SidebarContentSelector';
+import {useContext} from 'react';
 
 
 export default function Product() {
+  const {openBar} = useContext(SidebarContext)
   const params = useParams();
-  const productId = params.id
-  console.log(productId)
-  const {data, error, isLoading} = useSWR(API_URL + '/products/' + productId, authFetcher);
-  if (error) return <div>Failed to load</div>;
-  if (isLoading) return <div>Loading...</div>;
-  console.log(data);
-  return <div>
-    <ul>
-      {data && <li
-        key={data.id}>{data.attributes.name}, {data['attributes']['description']}</li>}
-    </ul>
+  const productId = params.id;
+  const {data: product, included: details, error, isLoading} = useProduct(productId);
+  const content = {
+    linkUrl: '/products/' + productId + '/details/create',
+    linkText: 'Add Details'
+  };
+  return <div className="relative h-full flex-col">
+    <ButtonLink
+      href={content.linkUrl}
+      size="small"
+      intent="primary"
+    >
+      {content.linkText}
+    </ButtonLink>
+    <div>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Failed to load</div>}
+      {product && (
+        <ul>
+          <li key={product.id}>
+            <ProductItem product={product}/>
+          </li>
+        </ul>
+      )}
+
+    </div>
+    <div className="grow flex-1">
+      <Card>
+        <ProductDetailsTable details={details}/>
+      </Card>
+    </div>
+
+
+    <SidebarContainer>
+      <SidebarContentSelector target={openBar.target}/>
+    </SidebarContainer>
   </div>;
 }
