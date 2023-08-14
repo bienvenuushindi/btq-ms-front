1b46e42a0c74347ee92559696a074f42878d7e86
usbbush <jeanbienvenusb@gmail.com>
2023-08-14 19:26:04 +0200

    Update input class list

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ const Input = ({ className, ...props }) => {
   return (
     <input
       className={clsx(
-        "border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full",
+        "border-solid border-gray border-2 px-3 py-2 text-lg rounded-2xl w-full",
         className
       )}
       {...props}
