a84c843fa7ad84558cc10ccf7fe789750d82c9d4
usbbush <jeanbienvenusb@gmail.com>
2023-08-15 21:55:51 +0200

    Display requisition  items

--- src/app/(dashboard)/requisitions/page.tsx
+++ src/app/(dashboard)/requisitions/page.tsx
@@ -1,9 +1,20 @@
+'use client';
 import CreateRequisition from '@/components/CreateRequisition';
+import {useRequisitions} from '@/app/hooks/useRequisitions';
+import RequisitionsList from '@/components/requisitions/RequisitionsList';
 
-export default function Requisitions(){
+export default function Requisitions() {
+  const {data, meta, links, error, isLoading} = useRequisitions();
   return (
     <>
-      <CreateRequisition/>
+      {isLoading && !error ? (<div>Loading...</div>) :
+        error ? <div>Failed to load</div> :
+          <div>
+            <CreateRequisition/>
+            <div>
+              <RequisitionsList items={data}/>
+            </div>
+          </div>}
     </>
   );
 }
