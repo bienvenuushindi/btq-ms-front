729bd322552158b0d45e18017dd4af90e8b8ed8a
usbbush <jeanbienvenusb@gmail.com>
2023-08-15 21:52:39 +0200

    Reformat code style

--- src/app/hooks/useFetcher.ts
+++ src/app/hooks/useFetcher.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 import {API_URL, authFetcher} from '@/lib/api';
 
 export const useFetcher = (url) => {
-  const {data: result = {}, error, isLoading, mutate} = useSWR(API_URL+url, authFetcher);
+  const {data: result = {}, error, isLoading, mutate} = useSWR(API_URL + url, authFetcher);
   const {data, meta = {}, links = {}} = result;
   return {data, meta, links, error, isLoading, mutate};
 };
