b5b68b03120fe5194b9116b89525579adb20f624
usbbush <jeanbienvenusb@gmail.com>
2023-08-21 14:09:53 +0200

    Use shoe image component

--- src/components/sidebar/ProductDetailsSidebar.tsx
+++ src/components/sidebar/ProductDetailsSidebar.tsx
@@ -1,29 +1,15 @@
-'use client';
-import {useContext, useEffect, useState} from 'react';
+
+import {useContext} from 'react';
 import {SidebarContext} from '@/components/sidebar/SidebarContainer';
-import Carousel from 'react-gallery-carousel';
 import 'react-gallery-carousel/dist/index.css';
-import {BASE_URL} from '@/lib/api';
+import ShowImages from '@/components/ShowImages';
 
 
 export default function ProductDetailsSidebar() {
-  const {openBar, sidebarData} = useContext(SidebarContext);
-  const [images, setImages] = useState([]);
-  const getImageUrls = () => {
-    return (sidebarData.image_urls || []).map((image_path) => ({
-      src: `${BASE_URL + image_path}`
-    }));
-  };
-
-  useEffect(() => {
-    if (openBar.state) {
-      const images_urls = getImageUrls();
-      setImages(images_urls);
-    }
-  }, [sidebarData]);
+  const {sidebarData} = useContext(SidebarContext);
   return (
     <>
-      {images.length === 0 ? <div>No Picture</div> : <Carousel images={images} style={{height: 400, width: '100%'}}/>}
+      <ShowImages/>
       {sidebarData && <div>Hello I am the product detail {sidebarData.size}</div> }
     </>
   );
