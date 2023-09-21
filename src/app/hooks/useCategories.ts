0ff94a06e8b9d311449b664801745874e8a24a2b
usbbush <jeanbienvenusb@gmail.com>
2023-09-21 11:53:21 +0200

    Update categories fetcher hook

--- src/app/hooks/useCategories.ts
+++ src/app/hooks/useCategories.ts
@@ -1,9 +1,10 @@
 'use client';
 import useSWR from 'swr';
 import {API_URL, authFetcher} from '@/lib/api';
+import useSWRImmutable from 'swr/immutable';
 
-export const useCategories = () => {
-  const {data: result={}, error, isLoading, mutate} = useSWR(API_URL + '/categories/', authFetcher);
-  const {data: categories=[], meta = {}, links = {}} = result;
-  return {categories , error, meta, links, isLoading,mutate};
+export const useCategories = ({url = null}) => {
+  const {data: result = {}, error, isLoading, mutate} = useSWRImmutable(url || API_URL + '/categories/', authFetcher);
+  const {data: categories = [], meta = {}, links = {}} = result;
+  return {categories, error, meta, links, isLoading, mutate};
 };
