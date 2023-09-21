38408fae196c729762a3379ec64c7945133b838f
usbbush <jeanbienvenusb@gmail.com>
2023-09-21 12:43:56 +0200

    Refactor Component

--- src/app/(dashboard)/categories/page.tsx
+++ src/app/(dashboard)/categories/page.tsx
@@ -5,13 +5,37 @@ import {useCategories} from '@/app/hooks/useCategories';
 import CategoriesHeader from '@/components/categories/CategoriesHeader';
 import {useState} from 'react';
 import EntityTable from '@/components/EntityTable';
-import {categoryColumns} from '@/components/categories/CategoriesTable';
 import ErrorBoundary from '@/components/ErrorBoundary';
+import Badge from '@/components/Badge';
 
 export default function Categories() {
   const [url, setUrl] = useState(null);
   const {categories, meta, links, error, isLoading, mutate} = useCategories(url);
+  const categoryColumns = [
+    {
+      key: 'name',
+      type: 'text',
+      label: 'Category Name',
+    }, {
+      key: 'description',
+      type: 'text',
+      label: 'Description',
+    }, {
+      key: 'active',
+      type: 'text',
+      label: ' Status',
+      dataTransformation: (value: any) => value ? <Badge variant="success">Active</Badge> : <Badge variant="danger">Inactive</Badge>,
+    }, {
+      key: 'created_at',
+      type: 'text',
+      label: 'Created',
 
+    }, {
+      key: 'count_products',
+      type: 'text',
+      label: 'Numb of Products',
+    },
+  ];
   return (
     <Container>
       <CategoriesHeader revalidate={mutate}/>
--- src/app/(dashboard)/requisitions/page.tsx
+++ src/app/(dashboard)/requisitions/page.tsx
@@ -1,20 +1,74 @@
 'use client';
-import CreateRequisition from '@/components/CreateRequisition';
 import {useRequisitions} from '@/app/hooks/useRequisitions';
 import RequisitionsList from '@/components/requisitions/RequisitionsList';
+import ContainerOne from '@/components/ContainerOne';
+import Container from '@/components/Container';
+import RequisitionsHeader from '@/components/requisitions/RequisitionsHeader';
+import ErrorBoundary from '@/components/ErrorBoundary';
+import EntityTable from '@/components/EntityTable';
+import {useRouter} from 'next/navigation';
+import Badge from '@/components/Badge';
 
 export default function Requisitions() {
-  const {data, meta, links, error, isLoading} = useRequisitions();
+  const {requisitions, meta, links, error, isLoading} = useRequisitions();
+  const router = useRouter()
+  const columns = [
+    {
+      key: 'date',
+      label: 'Date',
+      type: 'text'
+    },
+    {
+      key: 'total_price',
+      label: 'Total Amount',
+      type: 'text'
+    },
+    {
+      key: 'price_currency',
+      label: 'Currency',
+      type: 'text'
+    },
+    {
+      key: 'count_products',
+      label: 'No of Products ',
+      type: 'text'
+    },
+    {
+      key: 'count_products_bought',
+      label: 'Products Bought',
+      type: 'text'
+    },
+    {
+      key: 'archived',
+      label: 'Archived',
+      type: 'text',
+      dataTransformation: (value) => {
+        return value ? <Badge variant="success">Archived</Badge> : <Badge variant="danger">No</Badge>
+      }
+    },
+    {
+      key: 'Show',
+      type: 'button',
+      label: 'Show',
+      action: async (data) => {
+        await router.replace('/requisitions/' + data.id)
+      }
+    }
+  ];
   return (
-    <>
-      {isLoading && !error ? (<div>Loading...</div>) :
-        error ? <div>Failed to load</div> :
-          <div>
-            <CreateRequisition/>
-            <div>
-              <RequisitionsList items={data}/>
-            </div>
-          </div>}
-    </>
+    <Container>
+      <RequisitionsHeader/>
+      <ContainerOne>
+        <ErrorBoundary error={error}>
+          <EntityTable
+            isLoading={isLoading}
+            meta={meta}
+            links={links}
+            data={requisitions}
+            columns={columns}
+            entities={'requisitions'}/>
+        </ErrorBoundary>
+      </ContainerOne>
+    </Container>
   );
 }
