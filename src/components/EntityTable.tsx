021e649935f794469a2089fc19364d06c2f01345
usbbush <jeanbienvenusb@gmail.com>
2023-09-21 15:27:30 +0200

    Add props to toogle search component

--- src/components/EntityTable.tsx
+++ src/components/EntityTable.tsx
@@ -5,21 +5,21 @@ import {SearchBar} from '@/components/SearchBar';
 import DataGrid from '@/components/DataGrid';
 import Paginate from '@/components/Paginate';
 
-const EntityTable = ({ entities, updateList, meta, columns, data, isLoading, loader, links }) => {
+const EntityTable = ({entities, updateList, meta, columns, data, isLoading, loader, links, searchable = true}) => {
   return (
     <>
-      <SearchBar updateList={updateList} submitTo={`${API_URL}/${entities}`}/>
-      <TableMetaData meta={meta} />
+      {searchable && <SearchBar updateList={updateList} submitTo={`${API_URL}/${entities}`}/>}
+      <TableMetaData meta={meta}/>
       <DataGrid
         columns={columns}
         data={data}
-        tHeadProps={{ color: 'primary' }}
+        tHeadProps={{color: 'primary'}}
         isLoading={isLoading}
         loader={loader}
       />
-      <div className='flex justify-between w-full my-2'>
-        <TableMetaData meta={meta} />
-        <Paginate meta={meta} links={links} setUrl={updateList} />
+      <div className="flex justify-between w-full my-2">
+        <TableMetaData meta={meta}/>
+        <Paginate meta={meta} links={links} setUrl={updateList}/>
       </div>
     </>
   );
