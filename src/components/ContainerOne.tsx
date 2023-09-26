8f64b776d3b185d5b19c5b7b4feea28b0686b8e4
usbbush <jeanbienvenusb@gmail.com>
2023-09-26 12:32:36 +0200

    Update styles classlist

--- src/components/ContainerOne.tsx
+++ src/components/ContainerOne.tsx
@@ -1,6 +1,6 @@
 export default function ContainerOne({children}) {
   return (
-    <div className="w-full h-full container py-8 mx-auto flex flex-col items-start">
+    <div className="w-full h-full container py-4 mx-auto flex flex-col items-start">
           {children}
     </div>
   )
