87606b4692be164f01a5101b3423dde5d7bcb5a5
usbbush <jeanbienvenusb@gmail.com>
2023-09-26 12:35:56 +0200

    Update component wrapper

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ErrorBoundary = ({ error, children }) => {
   return (
-    <div>
+    <>
       {error ? <div>Failed to load</div> : children}
-    </div>
+    </>
   );
 };
 
