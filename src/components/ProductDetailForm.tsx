b3c963a1660cf5ddcc64811bc7da88a24466ee56
usbbush <jeanbienvenusb@gmail.com>
2023-09-26 12:40:53 +0200

    Update wrapper component and Add new field

--- src/components/ProductDetailForm.tsx
+++ src/components/ProductDetailForm.tsx
@@ -4,6 +4,8 @@ import Card from '@/components/Card';
 import {useRouter, useParams} from 'next/navigation';
 import {send} from '@/lib/api';
 import Form from '@/components/Form';
+import SearchTagBox from '@/components/tags/SearchTag';
+import ContainerOne from '@/components/ContainerOne';
 
 export const ProductDetailForm = () => {
   const router = useRouter();
@@ -17,7 +19,9 @@ export const ProductDetailForm = () => {
     dozen_units: 12,
     box_units: 1,
     currency: '',
-    active: ''
+    active: '',
+    tag_list: '',
+    supplier_id: null,
   };
   const [formState, setFormState] = useState({...initial});
   const [error, setError] = useState('');
@@ -41,6 +45,7 @@ export const ProductDetailForm = () => {
       setFormState({...initial});
     }
   };
+
   const productDetailForm = [
     {
       label: 'Size',
@@ -69,6 +74,72 @@ export const ProductDetailForm = () => {
       },
     },
     {
+      label: 'Choose currency',
+      placeholder: 'Select Currency',
+      name: 'currency',
+      input_type: 'radio',
+      className: '',
+      options: ['fc', 'ugx', 'usd'],
+      action: (e) => {
+        setFormState((s) => ({...s, currency: e.target.value}));
+      }
+    },
+    [
+      {
+        label: 'Box Price',
+        required: true,
+        placeholder: 'Box Price',
+        value: formState.dozen_price,
+        name: 'box-price',
+        input_type: 'number',
+        type: 'number',
+        className: '',
+        action: (e) => {
+          setFormState((s) => ({...s, box_price: e.target.value}));
+        },
+      },{
+      label: 'Unit Qty in Box',
+      required: true,
+      placeholder: 'Box Units',
+      value: formState.dozen_price,
+      name: 'box-units',
+      input_type: 'number',
+      type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, box_units: e.target.value}));
+      },
+    },
+    ],
+    [
+      {
+        label: 'Dozen Price',
+        required: true,
+        placeholder: 'Dozen Price',
+        value: formState.dozen_price,
+        name: 'dozen-price',
+        input_type: 'number',
+        type: 'number',
+        className: '',
+        action: (e) => {
+          setFormState((s) => ({...s, dozen_price: e.target.value}));
+        },
+      },
+      {
+        label: 'Unit Qty in Dozen',
+        required: true,
+        placeholder: 'Dozen units',
+        value: formState.dozen_units,
+        name: 'dozen-units',
+        input_type: 'number',
+        type: 'number',
+        className: '',
+        action: (e) => {
+          setFormState((s) => ({...s, dozen_units: e.target.value}));
+        },
+      },
+    ],
+    {
       label: 'Unit Price',
       required: true,
       placeholder: 'Unit Price',
@@ -82,70 +153,21 @@ export const ProductDetailForm = () => {
       },
     },
     {
-      label: 'Dozen Price',
-      required: true,
-      placeholder: 'Dozen Price',
-      value: formState.dozen_price,
-      name: 'dozen-price',
-      input_type: 'number',
-      type: 'number',
+      label: 'Enter Some Tags ...',
+      required: false,
+      placeholder: 'Add tag',
+      tag_list: formState.tag_list,
+      suggestion: <SearchTagBox path='/tags/search'/> ,
+      input_type: 'tag',
+      name: 'tags',
       className: '',
-      action: (e) => {
-        setFormState((s) => ({...s, dozen_price: e.target.value}));
+      action: (tags) => {
+        setFormState((s) => ({...s, tag_list: tags.join(',')}));
       },
-    },
-    {
-      label: 'Box Price',
-      required: true,
-      placeholder: 'Box Price',
-      value: formState.dozen_price,
-      name: 'box-price',
-      input_type: 'number',
-      type: 'number',
-      className: '',
-      action: (e) => {
-        setFormState((s) => ({...s, box_price: e.target.value}));
-      },
-    },
-    {
-      label: 'Unit Qty in Dozen',
-      required: true,
-      placeholder: 'Dozen units',
-      value: formState.dozen_units,
-      name: 'dozen-units',
-      input_type: 'number',
-      type: 'number',
-      className: '',
-      action: (e) => {
-        setFormState((s) => ({...s, dozen_units: e.target.value}));
-      },
-    },
-    {
-      label: 'Unit Qty in Box',
-      required: true,
-      placeholder: 'Box Units',
-      value: formState.dozen_price,
-      name: 'box-units',
-      input_type: 'number',
-      type: 'number',
-      className: '',
-      action: (e) => {
-        setFormState((s) => ({...s, box_units: e.target.value}));
-      },
-    },
-    {
-      label: 'Choose currency',
-      placeholder: 'Select Currency',
-      name: 'country',
-      input_type: 'radio',
-      className: '',
-      options: ['fc', 'ugx', 'usd'],
-      action: (e) => {
-        setFormState((s) => ({...s, currency: e.target.value}));
-      }
     },
     {
       label: 'Photos',
+      name: 'photos',
       input_type: 'image-file',
       image_props: {photos, setPhotos}
     },
@@ -172,16 +194,14 @@ export const ProductDetailForm = () => {
     buttonText: 'Create'
   };
   return (
-    <>
-      <Card>
-        <div className="w-full">
+    <ContainerOne>
+        <div className="w-full lg:w-2/4 mx-auto">
           <div className="text-center">
             <h2 className="text-3xl mb-2 text-black">{content.header}</h2>
             <p className="tex-lg text-black/25">{content.subheader}</p>
           </div>
           <Form fields={productDetailForm} handleSubmit={handleSubmit}/>
         </div>
-      </Card>
-    </>
+    </ContainerOne>
   );
 };
