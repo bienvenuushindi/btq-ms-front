779616a3f0e277d6664a2c05a4bd6cd9ecbe51b2
usbbush <jeanbienvenusb@gmail.com>
2023-09-26 12:41:56 +0200

    Update wrapper component and Add new field

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,14 +1,15 @@
 'use client';
-import Card from '@/components/Card';
 import React, {useState} from 'react';
 import {countries} from '@/data/countries';
 import {send} from '@/lib/api';
 import {useRouter} from 'next/navigation';
 import Form from '@/components/Form';
+import SearchTagBox from '@/components/tags/SearchTag';
+import ContainerOne from '@/components/ContainerOne';
 
 export const ProductForm = () => {
   const router = useRouter();
-  const initial = {name: '', short_description: '', description: '', active: false, country_origin: ''};
+  const initial = {name: '', short_description: '', description: '', active: false, country_origin: '', tag_list: ''};
   const [formState, setFormState] = useState({...initial});
   const [error, setError] = useState('');
   const [photos, setPhotos] = useState([]);
@@ -88,14 +89,30 @@ export const ProductForm = () => {
       }
     },
     {
+      label: 'Enter Some Tags ...',
+      required: false,
+      name: 'tag_list',
+      placeholder: 'Add tag',
+      tag_list: formState.tag_list,
+      suggestion: <SearchTagBox path='/tags/search'/> ,
+      input_type: 'tag',
+      className: '',
+      action: (tags) => {
+        setFormState((s) => ({...s, tag_list: tags.join(',')}));
+      },
+    },
+    {
       label: 'Photos',
       input_type: 'image-file',
+      name: 'photos',
       image_props: {photos, setPhotos}
     },
     {
       label: 'Status ',
       input_type: 'checkbox',
       className: '',
+      labelClassName:'sr-only',
+      name: 'active',
       checked: formState.active,
       action: () => {
         setFormState((s) => ({...s, active: !formState.active}));
@@ -110,14 +127,16 @@ export const ProductForm = () => {
   ];
 
   return (
-    <Card>
-      <div className="w-full">
+    <ContainerOne>
+      <div className="w-full lg:w-2/4 mx-auto">
         <div className="text-center">
           <h2 className="text-3xl mb-2 text-black">{content.header}</h2>
           <p className="tex-lg text-black/25">{content.subheader}</p>
-          <Form handleSubmit={handleSubmit} fields={productForm}/>
+          <div className="mx-auto">
+            <Form handleSubmit={handleSubmit} fields={productForm}/>
+          </div>
         </div>
       </div>
-    </Card>
+    </ContainerOne>
   );
 };
