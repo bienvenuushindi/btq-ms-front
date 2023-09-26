3c5e134248ffffef781e7ed1c92c6581820249a8
usbbush <jeanbienvenusb@gmail.com>
2023-09-26 12:39:31 +0200

    Use form component to generate fields

--- src/components/PriceDetailForm.tsx
+++ src/components/PriceDetailForm.tsx
@@ -1,24 +1,23 @@
 'use client';
-import {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import Card from '@/components/Card';
-import Input from '@/components/Input';
-import Button from '@/components/Button';
-import {useRouter} from 'next/navigation';
 import {send} from '@/lib/api';
-import SelectInput from '@/components/SelectInput';
 import {useCountries} from '@/app/hooks/useCountries';
 import {useSuppliers} from '@/app/hooks/useSuppliers';
+import Form from '@/components/Form';
+import {SidebarContext} from '@/components/sidebar/SidebarContainer';
+import ContainerOne from '@/components/ContainerOne';
+import SelectSupplier from '@/components/requisitions/SelectSupplier';
 
 export const PriceDetailForm = ({productDetailID}) => {
-  const router = useRouter();
-
+  const {setOpenBar} = useContext(SidebarContext);
   const [s_country, setCountry] = useState(null);
   const initial = {
-    dozen: 0.0,
-    box: 0.0,
+    dozen: 0,
+    box: 0,
     supplier_id: null,
     product_detail_id: productDetailID,
-    currency: 'usd'
+    currency: 'usd',
   };
 
   const {countries, error: countryError, isLoading: isCountryLoading} = useCountries();
@@ -26,104 +25,87 @@ export const PriceDetailForm = ({productDetailID}) => {
   const [formState, setFormState] = useState({...initial});
   const [error, setError] = useState('');
   const handleSubmit = async (e) => {
-      e.preventDefault();
-      const data = {
-        price_detail: formState
-      };
-      try {
-        await send('/product_details/' + productDetailID + '/price_details', data);
-        router.replace('/products/details/' + productDetailID);
-      } catch (e) {
-        setError(`Could not create product`);
-      } finally {
-        setFormState({...initial});
-      }
+    e.preventDefault()
+    const formData = new FormData();
+    Object.keys(formState).forEach((key) => {
+      formData.append(`price_detail[${key}]`, formState[key]);
+    });
+
+    try {
+      await send('/product_details/' + productDetailID + '/price_details', formData);
+      setOpenBar({state: true, target: 'price_details'});
+    } catch (e) {
+      setError(`Could not create product`);
+    } finally {
+      setFormState({...initial});
     }
+  };
+
+  const selectSupplier=(id)=>{
+    setFormState((s)=>({...s,supplier_id:id}))
+  }
 
   const content = {
     header: 'Add price',
     subheader: '',
     buttonText: 'Create'
   };
+  const priceForm = [
+    {
+      label:'Select Supplier',
+      input_type:'custom',
+      component:   <SelectSupplier action={selectSupplier} />,
+    },
+    {
+      label: 'Price of dozen',
+      required: true,
+      placeholder: 'Price of dozen',
+      value: formState.dozen,
+      name: 'dozen',
+      type: 'number',
+      input_type: 'text',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, dozen: e.target.value}));
+      },
+    },
+    {
+      label: 'Price of a box',
+      required: true,
+      placeholder: 'Price of a box',
+      value: formState.box,
+      name: 'box',
+      input_type: 'text',
+      type: 'number',
+      className: '',
+      action: (e) => {
+        setFormState((s) => ({...s, box: e.target.value}));
+      },
+    },
+    {
+      label: 'Select currency',
+      required: true,
+      placeholder: 'Select currency',
+      name: 'currency',
+      input_type: 'select',
+      className: '',
+      options: [{code: 'fc', name: 'fc'}, {code: 'ugx', name: 'ugx'}, {code: 'usd', name: 'usd'}],
+      action: (e) => {
+        setFormState((s) => ({...s, currency: e.target.value}));
+      }
+    },
+    {
+      input_type: 'button',
+      className: '',
+      type: 'submit',
+      placeholder: content.buttonText
+    }
+  ];
   return (
-    <>
-      <Card>
-        <div className="w-full">
-          <div className="text-center">
-            <h2 className="text-3xl mb-2 text-black">{content.header}</h2>
-            <p className="tex-lg text-black/25">{content.subheader}</p>
-          </div>
-          <form onSubmit={handleSubmit} className="py-10 w-full">
-
-            <div className="mb-8">
-              <Input required
-                     placeholder="Price of dozen"
-                     value={formState.dozen}
-                     type="number"
-                     step="0.1"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, dozen: e.target.value}))}
-              />
-            </div>
-            <div className="mb-8">
-              <Input required
-                     placeholder="Price of a box"
-                     value={formState.box}
-                     type="number"
-                     step="0.1"
-                     className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                     onChange={(e) =>
-                       setFormState((s) => ({...s, box: e.target.value}))}
-              />
-            </div>
-            <div className="mb-8">
-              <SelectInput required
-                // placeholder="Product Description"
-                           className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                           onChange={(e) =>
-                             setFormState((s) => ({...s, currency: e.target.value}))}>
-                <option value="">Select currency</option>
-                {['fc', 'ugx', 'usd'].map(curr => <option key={curr}
-                                                          value={curr}>{curr}</option>)}
-              </SelectInput>
-            </div>
-            <div className="mb-8 gap-3 flex">
-              {countries.map(country => (
-                <div key={country.attributes.code}>
-                  <Input required
-                         placeholder="Price of a box"
-                         value={country.id}
-                         type="radio"
-                         name="country"
-                         step="0.1"
-                         className="border-solid border-gray border-2 text-lg  py-0 w-fit"
-                         onChange={(e) =>
-                           setCountry(e.target.value)}
-                  /> <span>{country.attributes.name}</span>
-                </div>
-              ))}
-            </div>
-            <div className="mb-8">
-              <SelectInput required
-                           className="border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
-                           onChange={(e) =>
-                             setFormState((s) =>{
-                               return ({...s, supplier_id: e.target.value})
-                             })}>
-                <option value="">Select supplier shop</option>
-                {suppliers.map(supplier => <option key={supplier.id}
-                                                   value={supplier.id}>{supplier.attributes.shop_name}</option>)}
-              </SelectInput>
-            </div>
-            <div>
-              <Button type="submit" intent="secondary">
-                {content.buttonText}
-              </Button>
-            </div>
-          </form>
-        </div>
-      </Card>
-    </>
+    <ContainerOne>
+      <div className="w-full">
+        <Form handleSubmit={handleSubmit} fields={priceForm}/>
+      </div>
+    </ContainerOne>
   );
 };
