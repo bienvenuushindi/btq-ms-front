8bfefd2f041dbe7b162bdf553b282f06b4a8f919
usbbush <jeanbienvenusb@gmail.com>
2023-09-26 12:31:20 +0200

    Update styles classlist

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,16 +1,17 @@
 import Button from '@/components/Button';
+import clsx from 'clsx';
 
 const Accordion = ({id,title, content, isOpen, toggleAccordion}) => {
   return (
-    <div className="p-1 flex flex-col  items-center  w-full">
+    <div className={clsx("pl-1 flex flex-col items-center w-full rounded")}>
       <Button
         size="small"
         intent={'text'}
-        className="flex justify-between items-center w-full  rounded"
+        className="flex justify-between items-start w-full rounded px-2"
         onClick={() => toggleAccordion(id)}
       >
         <span className="text-lg font-semibold">{title}</span>
-        <span className={`transform ${isOpen ? 'rotate-0' : 'rotate-180'} transition-transform`}>
+        <span className={`transform ${isOpen ? 'rotate-0' : 'rotate-180'} transition-transform border px-1 rounded bg-lightBlue-200 text-white`}>
           &#9660;
         </span>
       </Button>
