5e4be46abad2f35cf1ead30956c596abe8f3f268
usbbush <jeanbienvenusb@gmail.com>
2023-10-04 12:41:59 +0200

    Remove duplicated header

--- src/components/requisitions/RequisitionItem.tsx
+++ src/components/requisitions/RequisitionItem.tsx
@@ -1,8 +1,7 @@
 export default function RequisitionItem({requisition}){
 
   return(
-    <div>
-      <h1>Requisition Details</h1>
+
       <ul className="grid grid-cols-2 space-x-2">
         <li><span>Schedule Date: </span> {requisition.date}</li>
         {/*<li><span>Contains: </span> {requisition_items.attributes.count_products} Product(s)</li>*/}
@@ -10,6 +9,5 @@ export default function RequisitionItem({requisition}){
         <li><span>Total Price: </span>{requisition.total_price}</li>
         <li><span>Currency: </span>{requisition.price_currency}</li>
       </ul>
-    </div>
   )
 }
