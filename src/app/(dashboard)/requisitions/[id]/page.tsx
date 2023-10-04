4b5eb87810164c38e620da90bbf968394a7019ad
usbbush <jeanbienvenusb@gmail.com>
2023-10-04 12:35:04 +0200

    Use new banner loader

--- src/app/(dashboard)/requisitions/[id]/page.tsx
+++ src/app/(dashboard)/requisitions/[id]/page.tsx
@@ -11,33 +11,42 @@ import ContainerOne from '@/components/ContainerOne';
 import ReqItemProductList from '@/components/requisitions/ReqItemProductList';
 import ErrorBoundary from '@/components/ErrorBoundary';
 import Card from '@/components/Card';
+import GridLoader from '@/components/banners/GridLoader';
 
 export default function Requisition() {
   const {openBar} = useContext(SidebarContext);
   const params = useParams();
   const requisitionId = params.id;
   const {data: requisition_items, mutate, error, isLoading} = useFetcher('/requisitions/' + requisitionId);
-  console.log(requisition_items)
+  console.log(requisition_items);
   return (
     <Container>
       <RequisitionDetailsHeader revalidate={mutate}/>
-      <ErrorBoundary error={error}>
-        <ContainerOne>
-          <Card className="w-full">
-            {requisition_items && (<RequisitionItem requisition={requisition_items.attributes}/>)}
-          </Card>
-        </ContainerOne>
-        <ContainerOne>
-          {isLoading && <div>Loading...</div>}
+      <ContainerOne>
+        <ErrorBoundary error={error}>
+          {isLoading && (
+            <div className="w-full flex flex-col gap-6">
+              <Card className="w-full ">
+                <GridLoader cols={2}/>
+              </Card>
+              <Card className="w-full ">
+                <GridLoader rows={1} height={10} className='w-1/3'/>
+                <GridLoader rows={8} height={10}/>
+              </Card>
+            </div>
+          )
+          }
           {requisition_items && (
             <>
+              <Card className="w-full">
+                <RequisitionItem requisition={requisition_items.attributes}/>
+              </Card>
               <ReqItemProductList details={requisition_items.attributes.product_items} requisitionId={requisitionId}/>
               <SidebarContentSelector target={openBar.target}/>
             </>
           )}
-        </ContainerOne>
-      </ErrorBoundary>
-
+        </ErrorBoundary>
+      </ContainerOne>
     </Container>
   );
 }
