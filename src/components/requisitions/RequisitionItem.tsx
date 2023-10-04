05928ca324b021faa5acfb7e6e0a87141604978e
usbbush <jeanbienvenusb@gmail.com>
2023-10-04 21:08:43 +0200

    Display info using 2 rows (title, value)

--- src/components/requisitions/RequisitionItem.tsx
+++ src/components/requisitions/RequisitionItem.tsx
@@ -1,13 +1,14 @@
+import InfoItem from '@/components/InfoItem';
+
 export default function RequisitionItem({requisition}){
 
   return(
 
-      <ul className="grid grid-cols-2 space-x-2">
-        <li><span>Schedule Date: </span> {requisition.date}</li>
-        {/*<li><span>Contains: </span> {requisition_items.attributes.count_products} Product(s)</li>*/}
-        <li><span>Bought: </span>{requisition.count_products_bought} Product(s)</li>
-        <li><span>Total Price: </span>{requisition.total_price}</li>
-        <li><span>Currency: </span>{requisition.price_currency}</li>
+      <ul className="grid grid-cols-2">
+        <li><InfoItem label="Schedule Date" value={requisition.date}/></li>
+        <li><InfoItem label="Bought" value={requisition.count_products_bought+' Product(s)'}/></li>
+        <li><InfoItem label="Total Price" value={requisition.total_price}/></li>
+        <li><InfoItem label="Currency" value={requisition.price_currency}/></li>
       </ul>
   )
 }
