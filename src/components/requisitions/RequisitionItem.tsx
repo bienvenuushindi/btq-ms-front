e291443e9d95290472c786fc608e678d1d915b08
usbbush <jeanbienvenusb@gmail.com>
2023-10-06 10:03:59 +0200

    Add icons

--- src/components/requisitions/RequisitionItem.tsx
+++ src/components/requisitions/RequisitionItem.tsx
@@ -1,14 +1,30 @@
 import InfoItem from '@/components/InfoItem';
+import {Calendar, CreditCard, Globe, Layers} from 'react-feather';
 
-export default function RequisitionItem({requisition}){
+export default function RequisitionItem({requisition}) {
 
-  return(
-
-      <ul className="grid grid-cols-2">
-        <li><InfoItem label="Schedule Date" value={requisition.date}/></li>
-        <li><InfoItem label="Bought" value={requisition.count_products_bought+' Product(s)'}/></li>
-        <li><InfoItem label="Total Price" value={requisition.total_price}/></li>
-        <li><InfoItem label="Currency" value={requisition.price_currency}/></li>
-      </ul>
+  return (
+    <ul className="grid grid-cols-2">
+      <li>
+        <InfoItem
+        label={<><Calendar size={16}/>Schedule Date</>}
+        value={requisition.date}/>
+      </li>
+      <li>
+        <InfoItem
+          label={<><CreditCard size={16}/> Bought</>}
+          value={requisition.count_products_bought + ' Product(s)'}/>
+      </li>
+      <li>
+        <InfoItem
+          label={<><Layers size={16}/> Total Price</>}
+          value={requisition.total_price}/>
+      </li>
+      <li>
+        <InfoItem
+          label={<><Globe size={16}/> Currency</>}
+          value={requisition.price_currency}/>
+      </li>
+    </ul>
   )
 }
