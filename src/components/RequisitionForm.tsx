6c19f1557d6ff6edb172da7ad63181836f6d35f8
usbbush <jeanbienvenusb@gmail.com>
2024-01-27 08:32:18 +0200

    Update component to use RequisitionModal

--- src/components/RequisitionForm.tsx
+++ src/components/RequisitionForm.tsx
@@ -1,17 +1,10 @@
 'use client';
-// External libraries
+
 import React from 'react';
 import {Plus} from 'react-feather';
-// Components
-import Button from './Button';
-import {SearchBar} from '@/components/SearchBar';
-import ProductSearchResults from '@/components/products/ProductSearchResults';
-import ModalContent from '@/components/modal/ModalContent';
-import ModalHeader from '@/components/modal/ModalHeader';
-import ModalBody from '@/components/modal/ModalBody';
-import ModalFooter from '@/components/modal/ModalFooter';
-import ModalContainer from '@/components/modal/ModalContainer';
-import { useModal, useRequisitionForm } from '@/components/requisitions/hooks';
+import {useModal, useRequisitionForm} from '@/components/requisitions/hooks';
+import RequisitionModal from "@/components/requisitions/RequisitionModal";
+import Button from "@/components/Button";
 
 
 export default function RequisitionForm({requisitionID, revalidate}) {
@@ -42,39 +35,16 @@ export default function RequisitionForm({requisitionID, revalidate}) {
                 </Button>
             </div>
 
-            <ModalContainer
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-            >
-                <ModalContent>
-                    <ModalHeader closeModal={closeModal} title={'Add Product'}/>
-                    <ModalBody>
-                        <div className={' focus-within:shadow-lg'}>
-                            <SearchBar onSearch={updateParams}/>
-                            <div>
-                                {
-                                    (url ? <ProductSearchResults url={url} setItems={setItems}/> :
-                                        <span>Enter your query</span>)
-                                }
-                            </div>
-                        </div>
-                        {items.length === 0
-                            ? ''
-                            : <div>
-                                <span>{items.length} item(s)</span>
-                                <ul>
-                                    {itemsList}
-                                </ul>
-                            </div>}
-                    </ModalBody>
-                    <ModalFooter closeModal={closeModal}>
-                        <div className="flex items-center">
-                            <Button type="button" onClick={handleSubmit}>Add </Button>
-                        </div>
-                    </ModalFooter>
-                </ModalContent>
-
-            </ModalContainer>
+            <RequisitionModal
+                modalIsOpen={modalIsOpen}
+                closeModal={closeModal}
+                updateParams={updateParams}
+                url={url}
+                items={items}
+                itemsList={itemsList}
+                setItems={setItems}
+                handleSubmit={handleSubmit}
+            />
         </>
     );
 }
