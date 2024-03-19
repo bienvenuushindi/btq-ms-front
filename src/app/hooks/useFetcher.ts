857d7df946b2bfced5ab9e9fa60b5f53113eafcf
usbbush <jeanbienvenusb@gmail.com>
2024-03-19 11:52:05 +0200

    Remove repeated destructring

--- src/app/hooks/useFetcher.ts
+++ src/app/hooks/useFetcher.ts
@@ -1,8 +1,7 @@
-import {API_URL, authFetcher} from '@/lib/api';
+import {authFetcher} from '@/lib/api';
 import useSWRImmutable from 'swr/immutable';
 
-export const useFetcher = (path, url=null) => {
-  const {data: result = {}, error, isLoading, mutate} = useSWRImmutable(  url || API_URL + path, authFetcher);
-  const {data, meta = {}, links = {}} = result;
-  return {data, meta, links, error, isLoading, mutate};
+export const useFetcher = (url: string) => {
+  const {data: result = {}, error, isLoading, mutate} = useSWRImmutable(  url , authFetcher);
+  return {...result, error, isLoading, mutate};
 };
