e956ea217f7f7d76813405da639225384fc6c398
usbbush <jeanbienvenusb@gmail.com>
2024-03-20 12:54:29 +0200

    Remove unnecessary prop

--- src/components/requisitions/SuppliersSection.tsx
+++ src/components/requisitions/SuppliersSection.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Card from '@/components/Card';
 import Divider from '@/components/Divider';
 
-export default function SuppliersSection({action, productId,suppliers, supplierId}){
+export default function SuppliersSection({action, productId, supplierId}){
   return (
     <div className="w-72 px-2 rounded">
       <SelectSupplier action={action} productId={productId} supplierId={supplierId}/>
