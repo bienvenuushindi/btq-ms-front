d1158d5c6001481f6f75526f57873895d6ffb657
usbbush <jeanbienvenusb@gmail.com>
2024-03-20 12:48:50 +0200

    Update title section position

--- src/components/requisitions/SupplierList.tsx
+++ src/components/requisitions/SupplierList.tsx
@@ -6,126 +6,128 @@ import SelectSupplierLoader from '@/components/banners/SelectSupplierLoader';
 function SupplierList({isLoading, suppliers, selected, onUpdateSelected, title}) {
     return (
         <div className="w-full">
-            <h2 className="text-lg font-medium my-2">{title}</h2>
             {isLoading ? (
                 <SelectSupplierLoader/>
             ) : (
-                <RadioGroup value={selected} onChange={onUpdateSelected}>
-                    <RadioGroup.Label className="sr-only">{title}</RadioGroup.Label>
-                    <div className="w-full flex justify-start flex-col items-start">
-                        <div className="w-full">
-                            <div className="space-y-2">
-                                {suppliers.map((supplier) => {
-                                        const {
-                                            id,
-                                            shop_name,
-                                            address,
-                                        } = supplier;
+                <>
+                    <h5 className="font-light my-2 text-sm">{title}</h5>
+                    <RadioGroup value={selected} onChange={onUpdateSelected}>
+                        <RadioGroup.Label className="sr-only">{title}</RadioGroup.Label>
+                        <div className="w-full flex justify-start flex-col items-start">
+                            <div className="w-full">
+                                <div className="space-y-2">
+                                    {suppliers.map((supplier: { id: any; shop_name: any; address: any; }) => {
+                                            const {
+                                                id,
+                                                shop_name,
+                                                address: supplierAddress,
+                                            } = supplier;
 
-                                        const {
-                                            city,
-                                            country,
-                                            address1,
-                                            address2,
-                                            tel1,
-                                            tel2
-                                        } = address
-                                        return (
-                                            <RadioGroup.Option
-                                                key={id}
-                                                value={id}
-                                                className={({active, checked}) =>
-                                                    `${active ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300' : ''}
+                                            const {
+                                                city,
+                                                country,
+                                                address1,
+                                                address2,
+                                                tel1,
+                                                tel2
+                                            } = supplierAddress
+                                            return (
+                                                <RadioGroup.Option
+                                                    key={id}
+                                                    value={id}
+                                                    className={({active, checked}) =>
+                                                        `${active ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300' : ''}
                          ${checked ? 'bg-lightBlue-100  text-white' : 'bg-white'}
                           relative flex cursor-pointer rounded-lg px-5 py-4 shadow-sm focus:outline-none focus:border-none border`
-                                                }
-                                            >
-                                                {({active, checked}) => (
-                                                    <>
-                                                        <div className="flex w-full items-center justify-between">
-                                                            <div className="flex items-center">
-                                                                <div className="text-sm">
-                                                                    <RadioGroup.Label
-                                                                        as="p"
-                                                                        className={`font-medium  ${
-                                                                            checked ? 'text-white' : 'text-gray-900 font-extrabold'
-                                                                        }`}
-                                                                    >
-                                                                        {shop_name}
-                                                                    </RadioGroup.Label>
-                                                                    <RadioGroup.Description
-                                                                        as="span"
-                                                                        className={`inline ${
-                                                                            checked ? 'text-sky-100' : 'text-gray-500'
-                                                                        }`}
-                                                                    >
-                                                                        <div className="flex flex-col">
-                                                                            <h6 className={`${
-                                                                                checked ? 'text-sky-100' : 'text-gray-700'
-                                                                            }`}>Address:</h6>
-                                                                            <div className={`${
-                                                                                checked ? 'text-sky-100' : 'text-gray-800'
-                                                                            }`}>
-                                                                                {(country || 'Country') + '/ ' + (city || 'City')}
-                                                                            </div>
-                                                                            <ul className="">
-                                                                                <li className="list-tem ml-2">  <span
-                                                                                    className={`${
-                                                                                        checked ? 'text-sky-100' : 'text-gray-700'
-                                                                                    }`}>1. {address1 || 'Address'}</span>
-                                                                                </li>
-                                                                                {
-                                                                                    address2 && (
-                                                                                        <li className="list-tem ml-2">  <span
-                                                                                            className={`${
-                                                                                                checked ? 'text-sky-100' : 'text-gray-700'
-                                                                                            }`}>2. {address2 || null}</span>
-                                                                                        </li>
-                                                                                    )}
-                                                                            </ul>
-                                                                        </div>
-
-                                                                        <div className="flex ">
-                                                                            <h6 className={`${
-                                                                                checked ? 'text-sky-100' : 'text-gray-700'
-                                                                            }`}>Tel:</h6>
-                                                                            <div>
+                                                    }
+                                                >
+                                                    {({active, checked}) => (
+                                                        <>
+                                                            <div className="flex w-full items-center justify-between">
+                                                                <div className="flex items-center">
+                                                                    <div className="text-sm">
+                                                                        <RadioGroup.Label
+                                                                            as="p"
+                                                                            className={`font-medium  ${
+                                                                                checked ? 'text-white' : 'text-gray-900 font-extrabold'
+                                                                            }`}
+                                                                        >
+                                                                            {shop_name}
+                                                                        </RadioGroup.Label>
+                                                                        <RadioGroup.Description
+                                                                            as="span"
+                                                                            className={`inline ${
+                                                                                checked ? 'text-sky-100' : 'text-gray-500'
+                                                                            }`}
+                                                                        >
+                                                                            <div className="flex flex-col">
+                                                                                <h6 className={`${
+                                                                                    checked ? 'text-sky-100' : 'text-gray-700'
+                                                                                }`}>Address:</h6>
+                                                                                <div className={`${
+                                                                                    checked ? 'text-sky-100' : 'text-gray-800'
+                                                                                }`}>
+                                                                                    {(country || 'Country') + '/ ' + (city || 'City')}
+                                                                                </div>
                                                                                 <ul className="">
-                                                                                    <li className="list-item ml-2"> <span
+                                                                                    <li className="list-tem ml-2">  <span
                                                                                         className={`${
                                                                                             checked ? 'text-sky-100' : 'text-gray-700'
-                                                                                        }`}>{tel1 || null}</span>
+                                                                                        }`}>1. {address1 || 'Address'}</span>
                                                                                     </li>
                                                                                     {
-                                                                                        (tel2) && (
-                                                                                            <li className="list-item ml-2"> <span
+                                                                                        address2 && (
+                                                                                            <li className="list-tem ml-2">  <span
                                                                                                 className={`${
                                                                                                     checked ? 'text-sky-100' : 'text-gray-700'
-                                                                                                }`}>{tel2 || null}</span>
+                                                                                                }`}>2. {address2 || null}</span>
                                                                                             </li>
                                                                                         )}
                                                                                 </ul>
                                                                             </div>
-                                                                        </div>
-                                                                    </RadioGroup.Description>
+
+                                                                            <div className="flex ">
+                                                                                <h6 className={`${
+                                                                                    checked ? 'text-sky-100' : 'text-gray-700'
+                                                                                }`}>Tel:</h6>
+                                                                                <div>
+                                                                                    <ul className="">
+                                                                                        <li className="list-item ml-2"> <span
+                                                                                            className={`${
+                                                                                                checked ? 'text-sky-100' : 'text-gray-700'
+                                                                                            }`}>{tel1 || null}</span>
+                                                                                        </li>
+                                                                                        {
+                                                                                            (tel2) && (
+                                                                                                <li className="list-item ml-2"> <span
+                                                                                                    className={`${
+                                                                                                        checked ? 'text-sky-100' : 'text-gray-700'
+                                                                                                    }`}>{tel2 || null}</span>
+                                                                                                </li>
+                                                                                            )}
+                                                                                    </ul>
+                                                                                </div>
+                                                                            </div>
+                                                                        </RadioGroup.Description>
+                                                                    </div>
                                                                 </div>
+                                                                {checked && (
+                                                                    <div className="shrink-0 text-white">
+                                                                        <CheckCircle className="h-6 w-6" color="#ffffff"/>
+                                                                    </div>
+                                                                )}
                                                             </div>
-                                                            {checked && (
-                                                                <div className="shrink-0 text-white">
-                                                                    <CheckCircle className="h-6 w-6" color="#ffffff"/>
-                                                                </div>
-                                                            )}
-                                                        </div>
-                                                    </>
-                                                )}
-                                            </RadioGroup.Option>
-                                        );
-                                    }
-                                )}
+                                                        </>
+                                                    )}
+                                                </RadioGroup.Option>
+                                            );
+                                        }
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </RadioGroup>
+                    </RadioGroup>
+                </>
             )}
         </div>
     );
