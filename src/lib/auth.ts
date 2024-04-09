abb5db77ff5139c3f400adbfdacf227687149a7c
usbbush <jeanbienvenusb@gmail.com>
2024-04-10 00:44:34 +0200

    Make file client component

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,4 @@
+'use client'
 import Cookies from 'js-cookie';
 
 export const setToken = (user: string, accessToken: string) => {
@@ -8,7 +9,6 @@ export const setToken = (user: string, accessToken: string) => {
     // Calculate the expiration time 1200 minutes (20 hours) from now
     const expiresDate = new Date();
     expiresDate.setTime(expiresDate.getTime() + 1200 * 60 * 1000); // 1200 minutes in milliseconds
-
     Cookies.set('user', user, { expires: expiresDate });
     Cookies.set('accessToken', accessToken, { expires: expiresDate });
     Cookies.set('expiresAt', expiresDate.toISOString(), { expires: expiresDate });
