6be781c864cd5151980d7316fed468cda54f51fb
usbbush <jeanbienvenusb@gmail.com>
2024-04-10 00:48:29 +0200

    Update css class

--- src/components/InfoItem.tsx
+++ src/components/InfoItem.tsx
@@ -7,11 +7,11 @@ import React from 'react';
 export default function InfoItem({label, value, children, classValue, classLabel, row = false, icon = null}:any) {
   return (
     <div className={clsx('flex pb-3', row ? 'flex-row' : 'flex-col')}>
-      <div className="flex gap-2 items-center w-fit text-gray-700 text-sm">
+      <div className="flex gap-2 items-center w-fit text-gray-700 text-sm border-b">
         {label}
       </div>
       {
-        value ? <Text size="large" intent="tertiary" className="font-bold">{value}</Text> : children
+        value ? <Text size="large" intent="tertiary" className={classValue || ''}>{value}</Text> : children
       }
     </div>
   );
