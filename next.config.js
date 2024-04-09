54a2c4a3efea2c3195f3af3a1e9430939e16b215
usbbush <jeanbienvenusb@gmail.com>
2024-04-10 00:42:17 +0200

    Add new image provider

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['127.0.0.1','res.cloudinary.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'flagsapi.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
     eslint: {
         ignoreDuringBuilds: true,
