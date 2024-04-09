42a3451c25554920209b5743ab9d26b530229840
usbbush <jeanbienvenusb@gmail.com>
2024-04-09 18:04:21 +0200

    Fixsidebar  animation

--- src/components/sections/sidebar/SidebarContainer.tsx
+++ src/components/sections/sidebar/SidebarContainer.tsx
@@ -22,12 +22,12 @@ export const SidebarContainer = ({title, children}:any) => {
           setOpenBar((prev) => ({...prev, state: false}));
         }}
         tabIndex={-1}
-        className={clsx('bg-[rgba(0,0,0,.4)] flex justify-center items-center absolute top-0 left-0 h-screen w-full transition-all ', !openBar.state && 'hidden')}
+        className={clsx('bg-[rgba(0,0,0,.4)] flex justify-center items-center absolute top-0 left-0 h-screen w-full', !openBar.state && 'hidden')}
         style={{height: `${maxHeight}px`}} // Set the height dynamically
       ></div>
 
       <div
-        className={clsx('bg-white fixed top-0 right-0 bottom-0 border-gray-700   lg:w-100 lg:shadow z-50 transition-all duration-1000 ease-in-out', !openBar.state && 'hidden', openBar.state ? 'translate-x-0' : 'translate-x-full')}
+        className={clsx('bg-white fixed top-0 right-0 bottom-0 border-gray-700   lg:w-100 lg:shadow z-50   ease-in-out delay-150 duration-300 ', openBar.state ? 'translate-x-0' : 'translate-x-full')}
         style={{height: '100%', minWidth: '400px'}}
       >
         <div className="shadow-sm p-1 gap-1  flex items-center">
@@ -49,7 +49,7 @@ export const SidebarContainer = ({title, children}:any) => {
         </div>
         <div className="pointer-events-auto">
           <div className="body relative h-[calc(100vh-10px)] overflow-y-auto">
-            {children}
+            {openBar.state && children}
           </div>
         </div>
       </div>
